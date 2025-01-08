093b99aeb01012608f7e99226a61800353358652
marven <100747377+Marv-07@users.noreply.github.com>
2025-01-08 18:36:57 +0900

    0108_Test
    Version_5.4.4
    DummyMap

---
+++ Source/DummyMap/DummyMap.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DummyMap.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DummyMap, "DummyMap" );
